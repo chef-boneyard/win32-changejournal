0f0f018ef7d72fb150290d671f0e7b1745178b88
Daniel Berger <djberg96@gmail.com>
2012-10-22 16:30:25 -0600

    Added initial set of constants.

--- lib/win32/changejournal/constants.rb
+++ lib/win32/changejournal/constants.rb
@@ -3,6 +3,12 @@ require 'ffi'
 module Windows
   module ChangeJournal
     module Constants
+      INVALID_HANDLE_VALUE = 0xFFFFFFFF
+      GENERIC_READ         = 0x80000000
+      GENERIC_WRITE        = 0x40000000
+      FILE_SHARE_READ      = 1
+      FILE_SHARE_WRITE     = 2
+      FILE_FLAG_OVERLAPPED = 0x40000000
     end
   end
 end
