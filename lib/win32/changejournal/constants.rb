fceb36108b6af41baf8ec12748cbe1ff179e7c8d
Daniel Berger <djberg96@gmail.com>
2012-10-22 17:09:29 -0600

    Added more constants and some initial functions.

--- lib/win32/changejournal/constants.rb
+++ lib/win32/changejournal/constants.rb
@@ -3,12 +3,23 @@ require 'ffi'
 module Windows
   module ChangeJournal
     module Constants
-      INVALID_HANDLE_VALUE = 0xFFFFFFFF
-      GENERIC_READ         = 0x80000000
-      GENERIC_WRITE        = 0x40000000
-      FILE_SHARE_READ      = 1
-      FILE_SHARE_WRITE     = 2
-      FILE_FLAG_OVERLAPPED = 0x40000000
+      INVALID_HANDLE_VALUE    = 0xFFFFFFFF
+      GENERIC_READ            = 0x80000000
+      GENERIC_WRITE           = 0x40000000
+      FILE_FLAG_OVERLAPPED    = 0x40000000
+      FILE_SHARE_READ         = 1
+      FILE_SHARE_WRITE        = 2
+      OPEN_EXISTING           = 3
+      FILE_DEVICE_FILE_SYSTEM = 0x00000009
+      METHOD_BUFFERED         = 0
+
+      def CTL_CODE(device, function, method, access)
+        ((device) << 16) | ((access) << 14) | ((function) << 2) | (method)
+      end
+
+      def FSCTL_QUERY_USN_JOURNAL
+        CTL_CODE(FILE_DEVICE_FILE_SYSTEM, 61, METHOD_BUFFERED, 0)
+      end
     end
   end
 end
--- lib/win32/changejournal/functions.rb
+++ lib/win32/changejournal/functions.rb
@@ -3,6 +3,16 @@ require 'ffi'
 module Windows
   module ChangeJournal
     module Functions
+      extend FFI::Library
+      ffi_lib :kernel32
+
+      attach_function :CloseHandle, [:ulong], :bool
+      attach_function :CreateFileW, [:buffer_in, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
+      attach_function :DeviceIoControl, [:ulong, :ulong, :buffer_in, :ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
+
+      ffi_lib FFI::Library::LIBC
+
+      attach_function :memset, [:pointer, :int, :size_t], :pointer
     end
   end
 end
