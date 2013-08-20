c827affac4f120abd762b1af7fdef1315680b115
Daniel Berger <djberg96@gmail.com>
2013-08-19 18:42:44 -0600

    Updated function prototypes.

--- lib/win32/changejournal/functions.rb
+++ lib/win32/changejournal/functions.rb
@@ -4,15 +4,44 @@ module Windows
   module ChangeJournal
     module Functions
       extend FFI::Library
+
+      def attach_pfunc(*args)
+        attach_function(*args)
+        private args[0]
+      end
+
+      typedef :uintptr_t, :handle
+      typedef :ulong, :dword
+      typedef :pointer, :ptr
+
       ffi_lib :kernel32
 
-      attach_function :CloseHandle, [:ulong], :bool
-      attach_function :CreateFileW, [:buffer_in, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
-      attach_function :DeviceIoControl, [:ulong, :ulong, :buffer_in, :ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
+      attach_pfunc :CloseHandle, [:handle], :bool
+      attach_pfunc :CreateFileW, [:buffer_in, :dword, :dword, :ptr, :dword, :dword, :handle], :handle
+      attach_pfunc :DeviceIoControl, [:handle, :dword, :buffer_in, :dword, :buffer_out, :dword, :ptr, :ptr], :bool
+      attach_pfunc :FormatMessage, :FormatMessageA, [:ulong, :ptr, :ulong, :ulong, :ptr, :ulong, :ptr], :ulong
 
       ffi_lib FFI::Library::LIBC
 
-      attach_function :memset, [:pointer, :int, :size_t], :pointer
+      attach_pfunc :memset, [:pointer, :int, :size_t], :pointer
+
+      # Returns a nicer Windows error message
+      def win_error(function, err=FFI.errno)
+        flags = 0x00001000 | 0x00000200
+        buf = FFI::MemoryPointer.new(:char, 1024)
+
+        FormatMessage(flags, nil, err , 0x0409, buf, 1024, nil)
+
+        function + ': ' + buf.read_string.strip
+      end
+
+      # Shortcut for win_error with raise builtin
+      def raise_windows_error(function, err=FFI.errno)
+        raise SystemCallError.new(win_error(function, err), err)
+      end
+
+      module_function :win_error
+      module_function :raise_windows_error
     end
   end
 end
