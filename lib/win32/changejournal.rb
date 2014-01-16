8803b085eceba3986596e3e2b14e3be470b48391
Daniel Berger <djberg96@gmail.com>
2014-01-16 13:31:34 -0700

    Use asynchronous code, fix folder name.

--- lib/win32/changejournal.rb
+++ lib/win32/changejournal.rb
@@ -16,13 +16,14 @@ module Win32
     def initialize(path, host = Socket.gethostname)
       @path = path.tr("/", "\\")
       @host = host
-      @conn = "winmgmts://#{host}/root/cimv2"
+      @conn = "winmgmts:{impersonationlevel=impersonate}!//#{host}/root/cimv2"
     end
 
-    def wait(seconds = nil)
+    def wait(seconds = 2)
       ole    = WIN32OLE.connect(@conn)
       drive  = @path.split(':').first + ":"
       folder = @path.split(':').last.gsub("\\", "\\\\\\\\")
+      folder << "\\\\" unless folder[-1] == "\\"
 
       query = %Q{
         select * from __instanceOperationEvent
@@ -32,12 +33,39 @@ module Win32
         and targetInstance.Path='#{folder}'
       }
 
-      events = ole.execNotificationQuery(query)
+      sink  = WIN32OLE.new('WbemScripting.SWbemSink')
+      event = WIN32OLE_EVENT.new(sink)
 
-      while event = events.nextEvent
-        p event.path_
-        p event.path_.class
+      ole.execNotificationQueryAsync(sink, query)
+      sleep 0.5
+
+      event.on_event("OnObjectReady"){ |object, context|
+        #object.TargetInstance.Properties_.each{ |p|
+        #  p p.Name
+        #}
+        puts "Modification of " + object.TargetInstance.Name
+      }
+
+      loop do
+        WIN32OLE_EVENT.message_loop
       end
+
+      #while true
+        #WIN32OLE_EVENT.message_loop
+        #while event = events.nextEvent
+        #  target = event.TargetInstance
+        #  p target.Name
+        #  p event.path_
+        #  p event.path_.class
+        #end
+      #end
     end
   end
+end
+
+if $0 == __FILE__
+  #https://social.technet.microsoft.com/Forums/scriptcenter/en-US/445f54d2-3b0c-4984-86e0-22f5734b368a/vbscript-wmi-filesystemwatcher?forum=ITCG
+  include Win32
+  cj = ChangeJournal.new("C:/Users/djberge")
+  cj.wait
 end
